2cd89277d038cad39dd3afc6d2de3f6d9b22e84b
Pedro Abreu <abreu223@hotmail.com>
2025-06-27 18:37:42 -0300

    Small bug in CMP

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -140,8 +140,8 @@ lazy_static! {
         /* Compare X and Y */
         OpCode::new(0xC9, "CMP", 2, 2,          AddressingMode::Immediate),
         OpCode::new(0xC5, "CMP", 2, 3,          AddressingMode::ZeroPage),
-        OpCode::new(0xD5, "CMP", 2, 4,          AddressingMode::Absolute),
-        OpCode::new(0xCD, "CMP", 3, 4,          AddressingMode::Immediate),
+        OpCode::new(0xD5, "CMP", 2, 4,          AddressingMode::ZeroPage_X),
+        OpCode::new(0xCD, "CMP", 3, 4,          AddressingMode::Absolute),
         OpCode::new(0xDD, "CMP", 3, 4 /* +1 */, AddressingMode::ZeroPage),
         OpCode::new(0xD9, "CMP", 3, 4 /* +1 */, AddressingMode::Absolute),
         OpCode::new(0xC1, "CMP", 2, 6,          AddressingMode::Indirect_X),
